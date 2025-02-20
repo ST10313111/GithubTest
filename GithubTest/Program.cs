bc683617ca7b80586a923b0a97837afbd1e41bbb
ST10313111 <126805359+ST10313111@users.noreply.github.com>
2025-02-20 15:28:50 +0200

    made messages longer

--- GithubTest/Program.cs
+++ GithubTest/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Kaeden");
+            Console.WriteLine("Kaeden just edited this file");
         }
     }
 }
